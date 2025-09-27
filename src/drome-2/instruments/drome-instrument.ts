649ae69b9eb6ebfd767d029dc93e16478abe8b52
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-09-27 08:30:22 -0400

    cleanup + fav

--- src/drome-2/instruments/drome-instrument.ts
+++ src/drome-2/instruments/drome-instrument.ts
@@ -224,7 +224,6 @@ class DromeInstrument<T extends number | number[]> {
   }
 
   reverb(mix: number, duration = 2) {
-    console.log(this.drome.reverbCache.get(`${mix}-${duration}`));
     this._reverb = new ReverbEffect(this.drome, { duration, mix });
     return this;
   }
